8d94cbc24f085d43e48fbd19ebe5ac219b561b82
unknown <ashutosh.nakhe@bridgera.com>
2022-12-05 08:45:15 +0530

    Added env-cmd dev dependency. Replaced hardcoded values with Environment variables.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const userRouter = require('./routers/user')
 const productRouter = require('./routers/product')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
@@ -26,17 +26,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Product = require('./models/product')
-const User = require('./models/user')
+// const Product = require('./models/product')
+// const User = require('./models/user')
 
-const main = async () => {
-    // const product = await Product.findById('5c2e505a3253e18a43e612e6')
-    // await product.populate('owner').execPopulate()
-    // console.log(product.owner)
+// const main = async () => {
+//     // const product = await Product.findById('5c2e505a3253e18a43e612e6')
+//     // await product.populate('owner').execPopulate()
+//     // console.log(product.owner)
 
-    // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-    // await user.populate('product').execPopulate()
-    // console.log(user.products)
-}
+//     // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+//     // await user.populate('product').execPopulate()
+//     // console.log(user.products)
+// }
 
-main()
+// main()
